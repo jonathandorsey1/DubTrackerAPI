5a6976fb9fce81b93c842b02ce038b7e3f3e7882
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-07 20:25:18 -0500

    first commit

---
+++ tracker_api/routes/main.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+
+from flask_qa.extensions import db
+from flask_qa.models import GameEntry
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    questions = Question.query.filter(Question.answer != None).all()
+
+    context = {
+        'questions' : questions
+    }
+
+    return render_template('home.html', **context)
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from tracker_api import create_app
+
+app = create_app()
