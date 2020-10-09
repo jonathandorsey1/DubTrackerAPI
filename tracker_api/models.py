70fba854eceded993a04b1a20d666286c44d6938
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-09 01:20:11 -0500

    track route

--- tracker_api/models.py
+++ tracker_api/models.py
@@ -3,8 +3,22 @@ from datetime import datetime
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(45), nullable=False)
+    username = db.Column(db.String(45), unique=True, nullable=False)
     games = db.relationship('PlayerGame', backref='player', lazy=True)
+    teams = db.relationship('TeamPlayer', backref='player', lazy=True)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dttm = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    # TODO: implement grabbing gamemode squad size
+    gamemode = db.Column(db.Integer, nullable=False)
+    entries = db.relationship('PlayerGame', backref='game', lazy=True)
+    teams = db.relationship('TeamGame', backref='game', lazy=True)
+    
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    players = db.relationship('TeamPlayer', backref='team', lazy=True)
+    games = db.relationship('TeamGame', backref='team', lazy=True)
 
 class PlayerGame(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,12 +27,14 @@ class PlayerGame(db.Model):
     kills = db.Column(db.Integer, nullable=False)
     damage = db.Column(db.Integer, nullable=False)
     deaths = db.Column(db.Integer, nullable=False)
+
+class TeamGame(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_team = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    id_game = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
     placement = db.Column(db.Integer, nullable=False)
 
-class Game(db.Model):
+class TeamPlayer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    dttm = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    # TODO: implement grabbing gamemode squad size
-    squad_size = db.Column(db.Integer)
-    entries = db.relationship('PlayerGame', backref='game', lazy=True)
-    
+    id_team = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    id_player = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
--- tracker_api/routes/main.py
+++ tracker_api/routes/main.py
@@ -1,14 +1,130 @@
 from flask import Blueprint, request
 
 from tracker_api.extensions import db
-from tracker_api.models import Player, PlayerGame, Game
+from tracker_api.models import *
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    wins = PlayerGame.query.filter_by(placement=1).group_by(id_game).all()
+    wins = TeamGame.query.filter_by(placement=1).all()
 
     return {
         'num wins': len(wins)
         }
+
+@main.route('/track', methods=['POST'])
+def track():
+    """
+    Expecting data in json format:
+    {
+        "game": {
+            "game_id": int,
+            "gamemode": int
+        },
+        "teams": [
+            {
+                "placement": int,
+                "members":
+                [
+                    
+                    {
+                        "username": string,
+                        "kills": int,
+                        "damage": int,
+                        "deaths": int
+                    },
+                ]
+            },
+        ]
+    }
+    """
+    payload = request.json
+    if 'game' not in payload or 'teams' not in payload:
+        return {
+            'status': 'Invalid payload!'
+        }
+
+    # get game from payload
+    game = get_game(payload)
+
+    # get players from teams
+    for _team in payload['teams']:
+        players = get_players(_team['members'], game)
+        # check if team record already exists
+        team = get_team(players)
+        team_game = TeamGame(
+            team=team,
+            game=game,
+            placement=_team['placement']
+        )
+        db.session.add(team_game)
+    db.session.commit()
+
+    return 'Game tracked!'
+
+def get_game(payload):
+    game = Game.query.filter_by(id=payload['game']['game_id']).first()
+    if game is None:
+        game = Game(
+            id=payload['game']['game_id'],
+            gamemode=payload['game']['gamemode']
+        )
+        db.session.add(game)
+    return game
+
+def get_players(team, game):
+    players = []
+    for _player in team:
+            # get player associated w/ game
+            player = Player.query.filter_by(username=_player['username']).first()
+            if player is None:
+                player = Player(
+                    username=_player['username'],
+                )
+                db.session.add(player)
+            players.append(player)
+    db.session.commit()
+    for player, _player in zip(players, team):
+        # add each PlayerGame record
+        player_game = PlayerGame.query.filter_by(id_player=player.id, id_game=game.id).first()
+        if player_game is None:
+            player_game = PlayerGame(
+                player=player,
+                game=game,
+                kills=_player['kills'],
+                damage=_player['damage'],
+                deaths=_player['deaths']
+            )
+            db.session.add(player_game)
+        # else:
+        #     # entry already exists, update
+        #     player_game['kills']=_player['kills'],
+        #     player_game['damage']=_player['damage'],
+        #     player_game['deaths']=_player['deaths']
+    db.session.commit()
+    return players
+
+def get_team(players):
+    # TODO: check if the following queries work
+    query = db.session.query(TeamPlayer).filter_by(id_player=players[0].id)
+
+    for player in players[1:]:
+        query = db.session.query(TeamPlayer).filter_by(id_player=player.id).join(query.subquery())
+    
+    record = query.first()
+    team = None
+    if record is None:
+        # make new team
+        team = Team()
+        db.session.add(team)
+        for player in players:
+            db.session.add(TeamPlayer(
+                team=team,
+                player=player
+            ))
+        db.session.commit()
+    else:
+        team = Team.query.filter(id=record.id_team)
+    
+    return team
