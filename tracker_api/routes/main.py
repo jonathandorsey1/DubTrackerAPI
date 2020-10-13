83fbf1ca91db6702489afb4c62a82fadb66f8e6d
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-12 22:00:40 -0500

    changed player wins

--- tracker_api/routes/main.py
+++ tracker_api/routes/main.py
@@ -25,15 +25,15 @@ def player_wins(username):
     team_players = player.teams
     teams = [team_player.team for team_player in team_players]
     wins = 0
-    subq = db.session.query(Game).filter_by(Game.gamemode.in_([1,2,3,4])).subquery()
+    # subq = db.session.query(Game).filter_by(Game.gamemode.in_([1,2,3,4])).subquery()
     for team in teams:
-        q = db.session.query(TeamGame).filter( \
-                (TeamGame.id_team==team.id_team) & \
-                (TeamGame.placement==1) & \
-                (TeamGame.game_id.in_(subq)) \
-                )
-        wins += len(q.all())
-        # wins += len(TeamGame.query.filter_by(id_team=team.id, placement=1).all())
+        # q = db.session.query(TeamGame).filter( \
+        #         (TeamGame.id_team==team.id_team) & \
+        #         (TeamGame.placement==1) & \
+        #         (TeamGame.game_id.in_(subq)) \
+        #         )
+        # wins += len(q.all())
+        wins += len(TeamGame.query.filter_by(id_team=team.id, placement=1).all())
 
 
     return {
