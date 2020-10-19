d713ffac7bfc65618b45a8ac4a5b303ccc88a970
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-19 18:50:03 -0500

    showing team attrs

--- tracker_api/routes/main.py
+++ tracker_api/routes/main.py
@@ -179,7 +179,7 @@ def get_team(players):
     
     for team in q.all():
         print('team id:',team.id_team)
-        print('team attrs:', team.__table__.columns)
+        print('team attrs:', team.items())
         print('team:', team)
     record = q.first()
     team = None
